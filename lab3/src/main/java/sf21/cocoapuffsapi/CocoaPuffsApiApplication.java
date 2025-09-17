fc69181478bd4c96b2caaf8b823cacb86b4c1a2f
DaviSFS21 <davisfs2110@gmail.com>
2025-09-16 22:26:43 -0300

    docs: update README with design patterns and anti-patterns

--- lab3/src/main/java/sf21/cocoapuffsapi/CocoaPuffsApiApplication.java
+++ lab3/src/main/java/sf21/cocoapuffsapi/CocoaPuffsApiApplication.java
@@ -2,7 +2,6 @@ package sf21.cocoapuffsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CocoaPuffsApiApplication {
